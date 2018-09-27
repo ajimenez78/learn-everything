e2aa2b9b8a5eba95325d1f607eb01254f4fa6ae8
Arturo Jiménez de los Galanes Reguillos <ajimenez78@gmail.com>
2018-09-27 17:48:36 +0200

    Prepare client-server framework for learning tool

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arturo/Dropbox/Proyectos/Pet/e-Learning/learn-everything/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
